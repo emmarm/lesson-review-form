df11ecd388f221ef3b2988259c189ce0492d3841
Emma M <mdopure@gmail.com>
2018-08-30 18:56:34 +0700

    mapped RangeInput to rating fields

--- src/components/LessonReviewForm.js
+++ src/components/LessonReviewForm.js
@@ -2,16 +2,39 @@ import React from 'react';
 
 import RangeInput from './RangeInput';
 
+const RATING_FIELDS = [{
+  inputId: 'fluency',
+  label: 'Fluency'
+}, {
+  inputId: 'pronun',
+  label: 'Pronunciation'
+}, {
+  inputId: 'comp',
+  label: 'Comprehension'
+}, {
+  inputId: 'vocab',
+  label: 'Active Vocabulary'
+}, {
+  inputId: 'grammar',
+  label: 'Active Grammar'
+}];
+
 class LessonReviewForm extends React.Component {
   state = {};
 
   render() {
     return (
       <form className="review-form">
-        <RangeInput
-          inputId="test-input"
-          label="Test"
-        />
+        {RATING_FIELDS.map(field => (
+          <RangeInput
+            key={field.inputId}
+            inputId={field.inputId}
+            className="review-form__field"
+            labelClass="review-form__label"
+            inputClass="review-form__input"
+            label={field.label}
+          />
+        ))}
       </form>
     );
   }
